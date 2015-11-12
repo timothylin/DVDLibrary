9525c0bb17c4c91a7b76e848753eb848e4de6066
timothylin <timothywlin@gmail.com>
2015-11-12 17:24:58 -0500

    removed unused method in BLL

--- DVDLibrary.BLL/MovieOperations.cs
+++ DVDLibrary.BLL/MovieOperations.cs
@@ -127,19 +127,5 @@ namespace DVDLibrary.BLL
 
             return _response;
         }
-
-        public bool ConfirmDelete(int movieID)
-        {
-            var movie = GetMovieByID(movieID);
-
-            if (movie.Success)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
     }
 }
