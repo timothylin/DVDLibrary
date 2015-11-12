6ff4956804859ba5a5fcc3dc57506fd4641573d8
paaaattighe <patrickstighe@outlook.com>
2015-11-12 17:23:24 -0500

    started borrower controller

---
+++ DVDLibrary.UI/Controllers/BorrowerController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DVDLibrary.UI.Controllers
+{
+    public class BorrowerController : Controller
+    {
+        // GET: Borrower
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        
+    }
+}
