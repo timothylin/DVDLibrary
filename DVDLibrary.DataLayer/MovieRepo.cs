a29120169845291ece575e814fde9fca93c119ed
koshin abdi <kabdi8@gmail.com>
2015-11-13 15:04:57 -0500

    added tests on data layer

--- DVDLibrary.DataLayer/MovieRepo.cs
+++ DVDLibrary.DataLayer/MovieRepo.cs
@@ -340,7 +340,7 @@ namespace DVDLibrary.DataLayer
             return Rentals;
         }
 
-        private List<Actor> GetListOfActorsByMovieID(int movieID)
+        public List<Actor> GetListOfActorsByMovieID(int movieID)
         {
             List<Actor> actors = new List<Actor>();
 
