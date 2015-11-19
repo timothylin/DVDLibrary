c3294f23a7411d1c1ffeddd5f9c452c90d01fbc8
timothylin <timothywlin@gmail.com>
2015-11-19 17:49:27 -0500

    tried adding jquery validation but its still not working

--- DVDLibrary.Models/MovieInfo.cs
+++ DVDLibrary.Models/MovieInfo.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Mvc;
@@ -11,11 +13,17 @@ namespace DVDLibrary.Models
     public class MovieInfo
     {
         public int MovieID { get; set; }
+        //[Required(ErrorMessage = "Please enter a title!")]
         public string Title { get; set; }
+        //[Required(ErrorMessage = "Please select a MPAA Rating!")]
         public MpaaRating MpaaRating { get; set; }
+        //[Required(ErrorMessage = "Please select a director!")]
         public Director Director { get; set; }
+        //[Required(ErrorMessage = "Please enter a release date!")]
         public int ReleaseDate { get; set; }
+        //[Required(ErrorMessage = "Please select a studio!")]
         public Studio Studio { get; set; }
+        //[Required(ErrorMessage = "Please select actors for the movie!")]
         public List<Actor> Actors { get; set; }
         public List<int> ActorIDs { get; set; } 
 
---
+++ DVDLibrary.UI/Scripts/app/addMovieValidation.js
@@ -0,0 +1,35 @@
+﻿$(document).ready(function () {
+    $('#addMovieForm').validate({
+        rules: {
+            Title: {
+                required: true
+            },
+            MpaaRating: {
+                required: true,
+            },
+            Director: {
+                required: true
+            },
+            ReleaseDate: {
+                required: true,
+                minlength: 4
+            },
+            Studio: {
+                required: true
+            },
+            Actors: {
+                required: true
+            }
+        },
+        messages: {
+            Title: "Please enter a title.",
+            MpaaRating: "Please select a rating.",
+            Director: "Please select a director.",
+            ReleaseDate: {
+                minlength: $.validator.format("Please enter in yyyy format!")
+            },
+            Studio: "Please select a studio.",
+            Actors: "Please select actors."
+        }
+    });
+});
